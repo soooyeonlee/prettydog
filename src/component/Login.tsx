e46d2d4ece2dad0908ee2d2aae7151641049f573
incar-kkb <incar.kkb@gmail.com>
2022-01-20 14:00:02 +0900

    20220120

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,18 +1,28 @@
 import { Button, Typography , Checkbox, Form, Input, Row, Card } from "antd";
 import { LoginDiv, LoginInnerDiv } from "./StyleComponet";
 import service, { ResponseDatas } from "../helper/service";
+import { ChangeEvent, useState } from "react";
 const { Title } = Typography;
 export default function Login(){
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
     const onFinish = (values: any) => {
         console.log('Success:', values);
         sendLogin();
     };
 
     const sendLogin = async() =>{
-        let result:ResponseDatas = await service('/login/token','POST', {'email':'test1@test.com', 'password':'test1234'});
-        if(result.status === 200 && result.data){
-            sessionStorage.setItem('token', Object(result.data).data.token);
-            document.location.href = "/home";
+        let result:ResponseDatas = await service('/login/token','POST', {'email':email, 'password':password});
+
+        if(Object(result.data).code !== '0'){
+            alert(Object(result.data).message);
+        }else{
+            if(result.status === 200 && result.data){
+                sessionStorage.setItem('token', Object(result.data).data.token);
+                document.location.href = "/home";
+            }
         }
     }
     
@@ -20,6 +30,13 @@ export default function Login(){
         console.log('Failed:', errorInfo);
     };
 
+    const onChangeEmail = (event : ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    }
+
+    const onChangePassword = (event : ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
     const validateMessages = {
         required: '${label} is required!',
         types: {
@@ -43,14 +60,14 @@ export default function Login(){
                     onFinishFailed={onFinishFailed}
                     autoComplete="off">
                     <Form.Item name={['user', 'email']} label="이메일" rules={[{ type: 'email',required:true, }]}>
-                        <Input />
+                        <Input value={email} onChange={onChangeEmail}/>
                     </Form.Item>
                     <Form.Item
                     label="비밀번호"
                     name="password"
                     rules={[{ required: true, message: 'Please input your password!' }]}
                 >
-                    <Input.Password />
+                    <Input.Password value={password} onChange={onChangePassword}/>
                     </Form.Item>
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
